f3445d3a0b2219deeba49ac36804a71ab324dea6
Shaik Hydumiya <shaikhydumiya1127@gmail.com>
2026-02-04 16:33:01 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -7,3 +7,12 @@ model = nn.Sequential(
 )
 X = torch.tensor([[0.,0.],[0.,1.],[1.,0.],[1.,1.]])
 y = torch.tensor([[0.],[1.],[0.]])
+loss_fn = nn.MSELoss()
+opt = torch.optim.Adam(model.parameters(),lr=0.01)
+for _ in range(1000):
+    opt.zero_grad()
+    y_pred = model(X)
+    loss = loss_fn(y_pred, y)
+    loss.backward()
+    opt.step()
+    print(model(X))
